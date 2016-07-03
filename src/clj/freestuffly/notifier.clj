c59903360d21f05c9a826fee8763fff45bcc774f
Emile <emile.swarts123@gmail.com>
2016-07-03 18:24:02 +0100

    fixup

--- src/clj/freestuffly/notifier.clj
+++ src/clj/freestuffly/notifier.clj
@@ -3,18 +3,14 @@
   (:require [postmark.core :as p])
   (:require [freestuffly.scraper :as scraper]))
 
-(def ^:private my-email
-  "emile.swarts123+heroku@gmail.com")
-
-(def ^:private subject
-  "Free stuffly finds")
-
+(def ^:private my-email "emile.swarts123+heroku@gmail.com")
+(def ^:private subject "Free stuffly finds")
 (def ^:private postmark
   (p/postmark
     (System/getenv "POSTMARK_API_KEY")
     (System/getenv "POSTMARK_SENDER_SIGNATURE")))
 
-(defn send-email
+(defn- send-email
   [content]
   (postmark {:to my-email
              :subject subject
